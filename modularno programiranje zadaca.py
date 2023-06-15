06100334c9406ba519b6d03088c221f7d9ccf529
Frano Vranjkoivić <134497804+fvranjkovic03@users.noreply.github.com>
2023-06-15 15:06:47 +0200

    Add files via upload

---
+++ modularno programiranje zadaca.py
@@ -0,0 +1,4 @@
+from rekurzija import ispisi_unatrag
+
+unos = input(" Unesite string: ")
+ispisi_unatrag(unos)
---
+++ rekurzija.py
@@ -0,0 +1,8 @@
+def ispisi_unatrag(string):
+    if len(string) == 0:
+        return
+    else:
+        ispisi_unatrag(string[1:])
+        print(string[0], end='')
+
+ispisi_unatrag("Rekurzija!")
